8cb3b39a8cbce6f33d130b70874c776ff7db3874
Artem Tverdokhliebov <artem.tverdokhlebov@gmail.com>
2021-02-20 17:28:28 +0200

    withoutBox in Display field

--- resources/views/form/display.blade.php
+++ resources/views/form/display.blade.php
@@ -8,12 +8,18 @@
     </label>
 
     <div class="{{$viewClass['field']}}">
+        @if($showBox)
         <div class="box box-solid box-default no-margin">
             <!-- /.box-header -->
             <div class="box-body">
-                {!! $value !!}&nbsp;
+        @endif
+
+        {!! $value !!}
+
+        @if($showBox)
             </div><!-- /.box-body -->
         </div>
+        @endif
 
         @if(\Illuminate\Support\Arr::get($help, 'position') == \Encore\Admin\Form\Field::HELP_UNDER_FIELD_POSITION)
             @include('admin::form.help-block')
--- src/Form/Field/Display.php
+++ src/Form/Field/Display.php
@@ -6,4 +6,16 @@ use Encore\Admin\Form\Field;
 
 class Display extends Field
 {
+    protected $showBox = true;
+
+    public function withoutBox() {
+        $this->showBox = false;
+    }
+
+    public function render()
+    {
+        return parent::fieldRender([
+            'showBox' => $this->showBox
+        ]);
+    }
 }
